d669bcd73c6b25890f22894b223d8f0860c16eac
Praneet Chakraborty <36084625+pran09@users.noreply.github.com>
2020-11-13 20:33:32 -0500

    commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,65 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
+var trm = trm || {};
 
-/** namespace. */
-var rhit = rhit || {};
+trm.ROSEFIRE_SECRET_KEY = 'ec8f39ee-23ee-48f0-b1dc-14c9a58a7fa2';
+trm.authManager = null;
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+trm.MainPageController = class {
 	constructor() {
-
+		document.querySelector('#navbarButton').onclick = (event) => {
+			trm.authManager.signIn();
+			this.updateView();
+		};
 	}
 
-	methodName() {
+	updateView() {
+		if (trm.authManager.isSignedIn) {
+			document.querySelector('#navbarButton').innerText = 'Tasks';
+		}
+	}
 
+}
+
+trm.AuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+
+	signIn() {
+		Rosefire.signIn(trm.ROSEFIRE_SECRET_KEY, (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+
+			firebase.auth().signInWithCustomToken(rfUser.token).catch(function(error) {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				if (errorCode == 'auth/invalid-custom-token') {
+					alert('The token you provided is invalid');
+				} else {
+					console.error()
+				}
+			});
+		});
+	}
+
+	get isSignedIn() {
+		return !!this._user;
 	}
 }
 
-/* Main */
-/** function and class syntax examples */
-rhit.main = function () {
+trm.main = function () {
 	console.log("Ready");
+	trm.authManager = new trm.AuthManager();
+	new trm.MainPageController();
 };
 
-rhit.main();
+trm.main();
